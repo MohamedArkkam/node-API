c91e497c7f7cc85498ce18855e905829031faf1e
Mohamed Arkkam <arkkam2000@gmail.com>
2024-07-22 16:13:40 +0530

    final

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const { timestamps } = require('mongodb')
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true, "Please enter the full name"]
+        },
+        quantity:{
+            type:Number,
+            required: true,
+            default: 0
+        },
+        price:{
+            type: Number,
+            required: true
+        },
+        image:{
+            type: String,
+            required: false
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+const product = mongoose.model("product", productSchema);
+module.exports = product
